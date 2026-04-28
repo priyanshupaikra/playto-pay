4dbcbd15f41054755f1e391fa8e8ddf2f4510e57
priyanshu paikra <priyanshu.paikra.com>
2026-04-28 21:41:12 +0530

    add accounts api url

--- backend/playto/urls.py
+++ backend/playto/urls.py
@@ -3,7 +3,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/', include('accounts.urls')),
     path('api/v1/', include('merchants.urls')),
     path('api/v1/', include('payouts.urls')),
     path('api/v1/', include('ledger.urls')),
 ]
+
